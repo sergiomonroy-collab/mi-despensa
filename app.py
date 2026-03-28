aca0cd22b7862fba1aeeff8420f56242c349707a
Alejandro <sergio.monroy.fragoso@gmail.com>
2026-03-28 12:07:51 -0600

    preparar para despliegue

--- app.py
+++ app.py
@@ -76,5 +76,9 @@ def registrar_compra():
     conn.close()
     return redirect(url_for('index'))
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Railway/Render asignará un puerto automáticamente, o usaremos el 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
